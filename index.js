f387805149b2c97023f1390ccd6697bb59ba017d
Jen W <jsc525@gmail.com>
2019-06-02 03:26:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 
 function addToCart(item) {
- var price = Math.ceil(Math.random() * 100);
+ var price = Math.ceil(Math.random() * 100) + 1;
   cart.push({[item]: price});
   return `${item} has been added to your cart.`;
 }
